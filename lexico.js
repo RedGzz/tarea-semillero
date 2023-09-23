647cfe6f378f6a6c90724834822362866ae3c7b8
rBlamed <74458156+RedGzz@users.noreply.github.com>
2023-09-23 09:15:44 -0500

    Add files via upload

---
+++ lexico.js
@@ -0,0 +1,40 @@
+function permutacionLexicografica(numeros) {
+    const n = numeros.length;
+    
+    while (true) {
+
+        console.log(numeros.join(' '));
+        
+
+        let i = n - 2;
+        while (i >= 0 && numeros[i] >= numeros[i + 1]) {
+            i--;
+        }
+        
+
+        if (i === -1) {
+            break;
+        }
+        
+
+        let j = n - 1;
+        while (numeros[j] <= numeros[i]) {
+            j--;
+        }
+        
+
+        [numeros[i], numeros[j]] = [numeros[j], numeros[i]];
+        
+
+        let left = i + 1;
+        let right = n - 1;
+        while (left < right) {
+            [numeros[left], numeros[right]] = [numeros[right], numeros[left]];
+            left++;
+            right--;
+        }
+    }
+}
+
+const numeros = [1, 2, 3, 4];
+permutacionLexicografica(numeros);
